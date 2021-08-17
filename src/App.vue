6a0a2adbbccdd23b24fe336f3b7733c4424f0f5f
Josh Cunha <joshua.cunha27@gmail.com>
2021-08-17 16:07:33 -0400

    made a skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Taskbar />
+  <HomePage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue';
+import Taskbar from './components/Taskbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,
+    Taskbar
   }
 }
 </script>
@@ -21,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="section style1">
+  </div>
+  <div class="section style2">
+  </div>
+  <div class="section style1">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.section {
+  height: 100vh;
+  width: 100%;
+  
+  &.style1 {
+    background-color: #faf3dd;
+  }
+  &.style2 {
+    background-color: #cef9f2;
+  }
+}
+</style>
